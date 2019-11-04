4f72ec31e5375c932e674e36da7db21aeb71f25c
Adisa Malik <adisamalik@gmail.com>
2019-11-04 00:15:59 -0500

    Added opacity transition

--- src/components/ProfileDropdown/ProfileDropdown.js
+++ src/components/ProfileDropdown/ProfileDropdown.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './ProfileDropdown.css'
 
 let ProfileDropdown = ({items, select}) => {
+    const [opacity, setOpacity] = useState(0);
+
+    useEffect(() => {
+        setOpacity(1);
+        return () => setOpacity(0);
+    }, [])
+    
     return (
-        <div className="ProfileDropdown">
+        <div className="ProfileDropdown" style={{opacity: opacity}}>
             <ul className="profile-dropdown-list">
                 {items.map((item) => <li key={item} onClick={e => select(item)} className="profile-dropdown-item">{item}</li>)}
             </ul>
