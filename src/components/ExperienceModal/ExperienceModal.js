c0293cc641b539dd38ab4b0e5a73ea0aa3498297
Adisa Malik <adisamalik@gmail.com>
2019-12-03 21:38:20 -0500

    Refactored FormModal

--- src/components/ExperienceModal/ExperienceModal.js
+++ src/components/ExperienceModal/ExperienceModal.js
@@ -9,11 +9,11 @@ import TextBox from '../TextBox';
 import { Formik, Form } from 'formik';
 import { ExperienceSchema } from '../../form-schemas';
 
-const ModalConsumer = ModalContext.Consumer;
 
 let ExperienceModal = ({edit, initial}) => {
     const months = ['Month', 'January', 'February', 'March', 'April', 'May',
     'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    
     let initialValues = {
         company: '', title: '', startMonth: 0, startYear: '', 
         endMonth: 0, endYear: '', current: false, description: '', tags: []
@@ -22,39 +22,28 @@ let ExperienceModal = ({edit, initial}) => {
     if (initial !== undefined) {
         initialValues = {...initialValues, ...initial}
     }
-    
-    return (
-        <ModalConsumer>
-        {({isOpen}) =>
-            (<FormModal title={(edit ? "Edit" : "Add") + " Experience"} showModal={isOpen} loading={false} delete={edit} enabled={true}>
-                <Formik
-                    initialValues={initialValues}
-                    validationSchema={ExperienceSchema}
-                >
-                    {({values}) => (
-                    <Form>
-                        <div className="form-row">
-                            <TextField label="Company name" id="company" inline={true}/>
-                            <TextField label="Title" id="title" inline={true} />
-                        </div>
-                        <div className="form-row">
-                            <SelectField name="Start date" id="startMonth" options={months} />
-                            <TextField label="Year" id="startYear" inline={true} />
-                            <div className="form-divider"/>
-                            <SelectField name="End date" id="endMonth" options={months} disabled={values.current} />
-                            <TextField label="Year" id="endYear" inline={true} disabled={values.current}/>
-                        </div>
-                        <div className="form-row">
-                            <CheckField desc="I'm currently working here" id="current"/>
-                        </div>
-                        <TagInput label="Skills" id="tags" values={values.tags} desc="Separate skils with commas"/>
-                        <TextBox label="Description" desc="Each line break will be bulleted separately" id="description"/>
-                    </Form>)}
-                </Formik>
-            </FormModal>)
-        }
-        </ModalConsumer>
-    );
-}
 
+    return (
+        <FormModal title={(edit ? "Edit" : "Add") + " Experience"} loading={false} delete={edit} enabled={true} initial={initialValues} schema={ExperienceSchema}>
+            {({values}) => (<div>
+                <div className="form-row">
+                    <TextField label="Company name" id="company" inline={true}/>
+                    <TextField label="Title" id="title" inline={true} />
+                </div>
+                <div className="form-row">
+                    <SelectField name="Start date" id="startMonth" options={months} />
+                    <TextField label="Year" id="startYear" inline={true} />
+                    <div className="form-divider"/>
+                    <SelectField name="End date" id="endMonth" options={months} disabled={values.current} />
+                    <TextField label="Year" id="endYear" inline={true} disabled={values.current}/>
+                </div>
+                <div className="form-row">
+                    <CheckField desc="I'm currently working here" id="current"/>
+                </div>
+                <TagInput label="Skills" id="tags" values={values.tags} desc="Separate skils with commas"/>
+                <TextBox label="Description" desc="Each line break will be bulleted separately" id="description"/>
+            </div>)}
+        </FormModal>
+    )
+}
 export default ExperienceModal;
