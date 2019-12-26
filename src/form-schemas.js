4bc7c5cde13071ee7866c14deaac590f58042042
Adisa Malik <adisamalik@gmail.com>
2019-12-25 20:52:37 -0500

    Updated date validation

--- src/form-schemas.js
+++ src/form-schemas.js
@@ -1,6 +1,5 @@
 import * as Yup from 'yup'
 
-
 export const ExperienceSchema = Yup.object().shape({
     company: Yup.string()
         .required('Company name is required'),
@@ -18,7 +17,18 @@ export const ExperienceSchema = Yup.object().shape({
     endMonth: Yup.number()
         .when('current', {
             is: false,
-            then: Yup.number().required('End month is required').integer().max(12),
+            then: Yup.number().required('End month is required').integer().max(12)
+            .test('date-test', 'Start date must be before end date',
+                function (value) {
+                    const { startYear, startMonth, endYear } = this.parent;
+                    if (startMonth !== 0 && startYear !== undefined && value !== 0 && endYear !== undefined)
+                    {
+                        let startDate = new Date(startYear, startMonth);
+                        let endDate = new Date(endYear, value);
+                        return startDate < endDate;
+                    }
+                    return true;
+                }),
             otherwise: Yup.number()
         }),
     endYear: Yup.number()
@@ -26,7 +36,6 @@ export const ExperienceSchema = Yup.object().shape({
             is: false,
             then: Yup.number().required('End year is required')
             .integer('Year is not valid')
-            .min(1900, 'Year is not valid')
             .max(new Date().getFullYear(), 'Year is not valid'),
             otherwise: Yup.number()
         }),
