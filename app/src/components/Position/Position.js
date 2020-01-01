c56ef38aa35f1809476cbaf4304b2730f287cefb
Adisa Malik <adisamalik@gmail.com>
2019-12-31 20:32:46 -0500

    Fix for date parsing

--- app/src/components/Position/Position.js
+++ app/src/components/Position/Position.js
@@ -8,9 +8,9 @@ import EducationModal from '../EducationModal';
 
 const ModalConsumer = ModalContext.Consumer;
 
-let Position = ({ startDate, endDate, ...props }) => {
+let Position = ({ startDate, endDate, id, createdAt, updatedAt, ...props }) => {
     let descItems = props.description.split('\n')
-    let options = { year: 'numeric', month: 'short' }
+    let options = { year: 'numeric', month: 'short', timeZone: 'UTC' }
     let modal;
     switch (props.type) {
         case 'experience':
@@ -25,12 +25,12 @@ let Position = ({ startDate, endDate, ...props }) => {
     }
 
     let info = {...props}
-    info.startMonth = startDate.getMonth() + 1
-    info.startYear = startDate.getFullYear()
+    info.startMonth = startDate.getUTCMonth() + 1
+    info.startYear = startDate.getUTCFullYear()
 
     if (endDate) {
-        info.endMonth = endDate.getMonth() + 1
-        info.endYear = endDate.getFullYear()
+        info.endMonth = endDate.getUTCMonth() + 1
+        info.endYear = endDate.getUTCFullYear()
     }
     else {
         info.current = true
@@ -40,14 +40,14 @@ let Position = ({ startDate, endDate, ...props }) => {
         <ModalConsumer>
         {({showModal}) => (
         <div className="Position">
-            <EditButton className="position-edit-button" onClick={() => showModal(modal, { edit: true, initial: info })}/>
+            <EditButton className="position-edit-button" onClick={() => showModal(modal, { edit: true, initial: info, id: id })}/>
             <div className="position-info">
                 <span className="position-title">{props.title} at </span><span className="position-company">{props.company}</span>
             </div>
             <div className="position-dates">{startDate.toLocaleDateString(undefined, options) + " - " + (endDate ? endDate.toLocaleDateString(undefined, options) : "Present")}</div>
             {props.tags.map((item) => <Tag key={item}>{item}</Tag>)}
             <ul className="position-description-container">
-                {descItems.map((item, index) => <li key={index} className="position-description">{item}</li>)}
+                {descItems.map((item, index) => item && <li key={index} className="position-description">{item}</li>)}
             </ul>
         </div>
         )}
