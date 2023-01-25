f8a6ec45a501814e4db5b9aaae8434dcf9b44e9e
Adisa Malik <adisamalik@gmail.com>
2023-01-25 02:40:37 -0500

    Linting

--- app/src/components/Position/Position.js
+++ app/src/components/Position/Position.js
@@ -1,75 +1,112 @@
-import React from 'react'
-import './Position.css'
-import Tag from '../Tag';
-import EditButton from '../EditButton';
-import { ModalContext } from '../ModalProvider/ModalProvider';
-import ExperienceModal from '../ExperienceModal';
-import EducationModal from '../EducationModal';
-import ProjectsModal from '../ProjectsModal/ProjectsModal';
+import React from "react";
+import "./Position.css";
+import Tag from "../Tag";
+import EditButton from "../EditButton";
+import { ModalContext } from "../ModalProvider/ModalProvider";
+import ExperienceModal from "../ExperienceModal";
+import EducationModal from "../EducationModal";
+import ProjectsModal from "../ProjectsModal/ProjectsModal";
 
 const ModalConsumer = ModalContext.Consumer;
 
-let Position = ({ startDate, endDate, id, createdAt, updatedAt, updateData, ...props }) => {
-    let descItems = props.description.split('\n')
-    let options = { year: 'numeric', month: 'short', timeZone: 'UTC' }
-    let modal = null;
-    let display = {
-        title: '',
-        org: '',
-        tags: []
-    }
+function Position({
+  startDate,
+  endDate,
+  id,
+  createdAt,
+  updatedAt,
+  updateData,
+  ...props
+}) {
+  const descItems = props.description.split("\n");
+  const options = { year: "numeric", month: "short", timeZone: "UTC" };
+  let modal = null;
+  const display = {
+    title: "",
+    org: "",
+    tags: [],
+  };
 
-    switch (props.type) {
-        case 'positions':
-            modal = ExperienceModal;
-            display.title = props.title;
-            display.org = props.company;
-            display.tags = props.tags;
-            break;
-        case 'education':
-            modal = EducationModal;
-            display.title = props.degree + ', ' + props.major;
-            display.org = props.school;
-            display.tags = props.tags;
-            break;
-        case 'projects':
-            modal = ProjectsModal
-            display.title = props.name;
-            display.tags = props.tags;
-            break;
-        default:
-            break;
-    }
+  switch (props.type) {
+    case "positions":
+      modal = ExperienceModal;
+      display.title = props.title;
+      display.org = props.company;
+      display.tags = props.tags;
+      break;
+    case "education":
+      modal = EducationModal;
+      display.title = `${props.degree}, ${props.major}`;
+      display.org = props.school;
+      display.tags = props.tags;
+      break;
+    case "projects":
+      modal = ProjectsModal;
+      display.title = props.name;
+      display.tags = props.tags;
+      break;
+    default:
+      break;
+  }
 
-    let info = {...props}
-    info.startMonth = startDate.getUTCMonth() + 1
-    info.startYear = startDate.getUTCFullYear()
+  const info = { ...props };
+  info.startMonth = startDate.getUTCMonth() + 1;
+  info.startYear = startDate.getUTCFullYear();
 
-    if (endDate) {
-        info.endMonth = endDate.getUTCMonth() + 1
-        info.endYear = endDate.getUTCFullYear()
-    }
-    else {
-        info.current = true
-    }
+  if (endDate) {
+    info.endMonth = endDate.getUTCMonth() + 1;
+    info.endYear = endDate.getUTCFullYear();
+  } else {
+    info.current = true;
+  }
 
-    return (
-        <ModalConsumer>
-        {({showModal}) => (
+  return (
+    <ModalConsumer>
+      {({ showModal }) => (
         <div className="Position">
-            <EditButton className="position-edit-button" onClick={() => showModal(modal, { edit: true, initial: info, id: id, updateData: updateData })}/>
-            <div className="position-info">
-                <span className="position-title">{display.title}{props.type !== 'projects' && ' at '}</span><span className="position-org">{display.org}</span>
-            </div>
-            <div className="position-dates">{startDate.toLocaleDateString(undefined, options) + " - " + (endDate ? endDate.toLocaleDateString(undefined, options) : "Present")}</div>
-            {display.tags.map((item) => <Tag key={item}>{item}</Tag>)}
-            <ul className="position-description-container">
-                {descItems.map((item, index) => item && <li key={index} className="position-description">{item}</li>)}
-            </ul>
+          <EditButton
+            className="position-edit-button"
+            onClick={() =>
+              showModal(modal, {
+                edit: true,
+                initial: info,
+                id,
+                updateData,
+              })
+            }
+          />
+          <div className="position-info">
+            <span className="position-title">
+              {display.title}
+              {props.type !== "projects" && " at "}
+            </span>
+
+            <span className="position-org">{display.org}</span>
+          </div>
+          <div className="position-dates">
+            {`${startDate.toLocaleDateString(undefined, options)} - ${
+              endDate
+                ? endDate.toLocaleDateString(undefined, options)
+                : "Present"
+            }`}
+          </div>
+          {display.tags.map((item) => (
+            <Tag key={item}>{item}</Tag>
+          ))}
+          <ul className="position-description-container">
+            {descItems.map(
+              (item, index) =>
+                item && (
+                  <li key={index} className="position-description">
+                    {item}
+                  </li>
+                )
+            )}
+          </ul>
         </div>
-        )}
-        </ModalConsumer>
-    )
+      )}
+    </ModalConsumer>
+  );
 }
 
-export default Position
+export default Position;
--- app/src/components/ProjectsModal/ProjectsModal.js
+++ app/src/components/ProjectsModal/ProjectsModal.js
@@ -1,84 +1,135 @@
-import React from 'react';
-import TextField from '../TextField';
-import SelectField from '../SelectField';
-import TagInput from '../TagInput';
-import TextBox from '../TextBox';
-import Modal from '../Modal';
-import FormFooter from '../FormFooter';
-import { Formik, Form } from 'formik';
-import { ModalContext } from '../ModalProvider/ModalProvider';
-import { ProjectSchema } from '../../form-schemas';
-import { updateItem, createItem, deleteItem } from '../../db-actions';
+import React from "react";
+import { Formik, Form } from "formik";
+import TextField from "../TextField";
+import SelectField from "../SelectField";
+import TagInput from "../TagInput";
+import TextBox from "../TextBox";
+import Modal from "../Modal";
+import FormFooter from "../FormFooter";
+import { ModalContext } from "../ModalProvider/ModalProvider";
+import { ProjectSchema } from "../../form-schemas";
+import { updateItem, createItem, deleteItem } from "../../db-actions";
 
 const ModalConsumer = ModalContext.Consumer;
 
-let ProjectsModal = ({edit, initial, updateData, id}) => {
-    const months = ['Month', 'January', 'February', 'March', 'April', 'May',
-    'June', 'July', 'August', 'September', 'October', 'November', 'December']
+function ProjectsModal({ edit, initial, updateData, id }) {
+  const months = [
+    "Month",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-    let initialValues = {
-        name: '', website: '', startMonth: 0, startYear: '', description: '', tags: []
-    }
+  let initialValues = {
+    name: "",
+    website: "",
+    startMonth: 0,
+    startYear: "",
+    description: "",
+    tags: [],
+  };
 
-    if (initial !== undefined) {
-        initialValues = {...initialValues, ...initial}
-    }
+  if (initial !== undefined) {
+    initialValues = { ...initialValues, ...initial };
+  }
 
-    return (
-        <ModalConsumer>
-            {({ isOpen, hideModal }) => (<Modal showModal={isOpen} submitted={false} title={(edit ? "Edit" : "Add") + " Project"}>
-                <Formik
-                    initialValues={initialValues}
-                    validationSchema={ProjectSchema}
-                    validateOnMount={true}
-                    onSubmit={(values, actions) => {
-                        let { startYear, startMonth, ...output } = values;
-                        output.startDate = new Date(values.startYear, values.startMonth - 1);
-                        
-                        if (edit) {
-                            output.id = id;
-                            updateItem(id, 'projects', output)
-                            .then(result => {
-                                updateData(prevData => prevData.map(item => {if (item.id === id) return output; return item}));
-                                actions.setSubmitting(false);
-                                hideModal();
-                            })
-                        } else {
-                            createItem('projects', output)
-                            .then(result => {
-                                updateData(prevData => [...prevData, result]);
-                                actions.setSubmitting(false);
-                                hideModal();
-                            })
-                        }
-                    }}
-                >
-                    {({ isValid }) => (
-                        <Form className="modal-inner">
-                            <div className="modal-body">
-                                <div className="modal-body-content">
-                                    <div className="form-row">
-                                        <TextField label="Project name" id="name" inline={true} />
-                                        <TextField label="Website" id="website" inline={true} />
-                                    </div>
-                                    <div className="form-row">
-                                        <SelectField name="Release date" id="startMonth" options={months} />
-                                        <TextField label="Year" id="startYear" inline={true} />
-                                    </div>
-                                    <TagInput label="Skills" id="tags" desc="Separate skils with commas" />
-                                    <TextBox label="Description" desc="Each line break will be bulleted separately" id="description" />
-                                </div>
-                            </div>
-                            <FormFooter loading={false} enabled={isValid} delete={edit} hide={hideModal} onDelete={() => deleteItem(id, 'projects')
-                            .then(result => {
-                                updateData(prevData => prevData.filter(item => item.id !== id));
-                                hideModal();
-                            })}/>
-                        </Form>
-                    )}
-                </Formik>
-            </Modal>)}
-        </ModalConsumer>
-    )
+  return (
+    <ModalConsumer>
+      {({ isOpen, hideModal }) => (
+        <Modal
+          showModal={isOpen}
+          submitted={false}
+          title={`${edit ? "Edit" : "Add"  } Project`}
+        >
+          <Formik
+            initialValues={initialValues}
+            validationSchema={ProjectSchema}
+            validateOnMount
+            onSubmit={(values, actions) => {
+              const { startYear, startMonth, ...output } = values;
+              output.startDate = new Date(
+                values.startYear,
+                values.startMonth - 1
+              );
+
+              if (edit) {
+                output.id = id;
+                updateItem(id, "projects", output).then((result) => {
+                  updateData((prevData) =>
+                    prevData.map((item) => {
+                      if (item.id === id) return output;
+                      return item;
+                    })
+                  );
+                  actions.setSubmitting(false);
+                  hideModal();
+                });
+              } else {
+                createItem("projects", output).then((result) => {
+                  updateData((prevData) => [...prevData, result]);
+                  actions.setSubmitting(false);
+                  hideModal();
+                });
+              }
+            }}
+          >
+            {({ isValid }) => (
+              <Form className="modal-inner">
+                <div className="modal-body">
+                  <div className="modal-body-content">
+                    <div className="form-row">
+                      <TextField label="Project name" id="name" inline />
+                      <TextField label="Website" id="website" inline />
+                    </div>
+                    <div className="form-row">
+                      <SelectField
+                        name="Release date"
+                        id="startMonth"
+                        options={months}
+                      />
+                      <TextField label="Year" id="startYear" inline />
+                    </div>
+                    <TagInput
+                      label="Skills"
+                      id="tags"
+                      desc="Separate skils with commas"
+                    />
+                    <TextBox
+                      label="Description"
+                      desc="Each line break will be bulleted separately"
+                      id="description"
+                    />
+                  </div>
+                </div>
+                <FormFooter
+                  loading={false}
+                  enabled={isValid}
+                  delete={edit}
+                  hide={hideModal}
+                  onDelete={() =>
+                    deleteItem(id, "projects").then((result) => {
+                      updateData((prevData) =>
+                        prevData.filter((item) => item.id !== id)
+                      );
+                      hideModal();
+                    })
+                  }
+                />
+              </Form>
+            )}
+          </Formik>
+        </Modal>
+      )}
+    </ModalConsumer>
+  );
 }
 export default ProjectsModal;
